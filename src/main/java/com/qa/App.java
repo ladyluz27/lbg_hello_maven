67a5e5608ba9a43c5960b03a2841518631b07be7
lschofield <lady.luz.27@gmail.com>
2025-09-10 10:53:19 +0100

    [EDIT] bump to v1.2 and changes

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Lucy"));
+        System.out.println(sayHelloToSomeone("Kerryn"));
         System.out.println(sayGoodbye());
     }
 
